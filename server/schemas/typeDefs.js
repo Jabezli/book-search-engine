b5cd5465690380f71fb50cdb50ba5464ee338ed1
Jabez <jabezli1990@gmail.com>
2023-05-31 22:53:06 -0700

    finished typeDefs.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
+    bookCount: Int
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(book: BookInput!): User
+    removeBook(bookId: String!): User
+  }
+
+  input BookInput {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+`;
+// why do we have the input here? cant we just use Book type?
+module.exports = typeDefs;
